ed0e7acca1f2cccf2fe0007d8104a893c59859e2
unixlinuxgeek <unixlinuxgeek@gmail.com>
2023-09-06 00:43:47 +0500

    Fix exercise 2.6

--- lessons/chapter2/2.6/README.md
+++ lessons/chapter2/2.6/README.md
@@ -1,2 +1,7 @@
 ### Пакеты и файлы
 
+Упражнение 2.1:
+
+[Условие](https://github.com/unixlinuxgeek/The_Go_Programming_Language/blob/8a43b19800df46066e6002537ed202b7b9bdc191/lessons/chapter2/2.6/exercise2.1/README.md)
+
+[Решение](https://github.com/unixlinuxgeek/The_Go_Programming_Language/blob/8a43b19800df46066e6002537ed202b7b9bdc191/lessons/chapter2/2.6/exercise2.1/tempconv.go) 
--- lessons/chapter2/2.6/tempconv.go
+++ lessons/chapter2/2.6/tempconv.go
@@ -4,13 +4,11 @@ import "fmt"
 
 type Cel float64
 type Far float64
-type Kel float64
 
 const (
 	AbsoluteZeroC Cel = -273.15
 	FreezingC     Cel = 0
 	BoilingC      Cel = 100
-	KelvinZeroC   Cel = -273.15
 )
 
 func (c Cel) String() string {
@@ -19,8 +17,4 @@ func (c Cel) String() string {
 
 func (f Far) String() string {
 	return fmt.Sprintf("%g ℉", f)
-}
-
-func (k Kel) String() string {
-	return fmt.Sprintf("%g °K", k)
 }
